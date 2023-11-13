ced7785a60c60c682b63d02966fbaa48fd005c2f
Justin Lee <140270202+JustinSteel@users.noreply.github.com>
2023-11-13 16:31:01 -0700

    add edit function to bulk discount

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -6,5 +6,44 @@ class BulkDiscountsController < ApplicationController
 
   def show
     @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
+
+  def new
+   @merchant = Merchant.find(params[:merchant_id])
+   @bulk_discount = @merchant.bulk_discounts.build
+  end
+
+  def create
+    @bulk_discount = BulkDiscount.create!(bulk_discount_params)
+    redirect_to merchant_bulk_discounts_path(@bulk_discount.merchant_id)
+  end
+
+  def edit
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
+
+  def update
+    if params[:percentage_discount].to_i > 100
+      flash.notice = "Discount cannot be greater than 100%"
+    else 
+      @merchant = Merchant.find(params[:merchant_id])
+      @bulk_discount = BulkDiscount.find(params[:id])
+      @bulk_discount.update(bulk_discount_params)
+      redirect_to merchant_bulk_discount_path(@merchant.id, @bulk_discount.id)
+    end
+  end
+
+  def destroy
+    @merchant = Merchant.find(params[:merchant_id])  
+    @bulk_discount = BulkDiscount.find(params[:id])
+    @bulk_discount.destroy
+    redirect_to merchant_bulk_discounts_path(@merchant)
+  end
+
+  private
+  def bulk_discount_params
+    params.require(:bulk_discount).permit(:percentage_discount, :quantity_threshold, :merchant_id)
   end
 end
--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,6 +1,5 @@
 class BulkDiscount < ApplicationRecord
   belongs_to :merchant
-  has_many :items, through: :merchant
   validates_presence_of :percentage_discount, :quantity_threshold
   
 end
